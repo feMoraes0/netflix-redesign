9b036aa03c8ac0e6e36a54810829574ea7165921
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-25 20:07:21 +0000

    feat: added continue watching

--- lib/tabs/shows.dart
+++ lib/tabs/shows.dart
@@ -12,6 +12,34 @@ class Shows extends StatelessWidget {
     {'name': 'The Last Summer', 'cover': 'images/trailer/trailer-005.jpeg'},
   ];
 
+  final List<Map> watching = [
+    {
+      'name': 'Outlaw King',
+      'subtitle': '60m of 137m',
+      'cover': 'images/netflix-cover/netflix-cover-004.jpg'
+    },
+    {
+      'name': 'Mowgli: Legend of the Jungle',
+      'subtitle': '11m of 104m',
+      'cover': 'images/netflix-cover/netflix-cover-001.jpg'
+    },
+    {
+      'name': 'Rim of the World',
+      'subtitle': '25m of 99m',
+      'cover': 'images/netflix-cover/netflix-cover-002.jpg'
+    },
+    {
+      'name': 'Revenger',
+      'subtitle': '130m of 142m',
+      'cover': 'images/netflix-cover/netflix-cover-003.jpg'
+    },
+    {
+      'name': 'The Night Comes for Us',
+      'subtitle': '10m of 121m',
+      'cover': 'images/netflix-cover/netflix-cover-005.jpg'
+    },
+  ];
+
   Widget titleSection(String text) {
     return Text(
       text,
@@ -41,6 +69,53 @@ class Shows extends StatelessWidget {
               ),
               child: this.titleSection('Continue Watching'),
             ),
+            Container(
+              height: 210.0,
+              margin: const EdgeInsets.only(bottom: 40.0),
+              padding: const EdgeInsets.only(left: 20.0),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: this.watching.length,
+                itemBuilder: (context, index) {
+                  Map element = this.watching[index];
+                  return Container(
+                    margin: const EdgeInsets.only(
+                      right: 15.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          height: 160.0,
+                          width: screen.width * 0.60,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(element['cover']),
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          element['name'],
+                          style: TextStyle(
+                            fontSize: 19.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          element['subtitle'],
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
             Padding(
               padding: EdgeInsets.only(
                 left: 20.0,
@@ -49,7 +124,8 @@ class Shows extends StatelessWidget {
               child: this.titleSection('Netflix Originals'),
             ),
             Container(
-              height: 250.0,
+              height: 200.0,
+              margin: const EdgeInsets.only(bottom: 35.0),
               padding: const EdgeInsets.only(left: 20.0),
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
@@ -57,7 +133,9 @@ class Shows extends StatelessWidget {
                 itemBuilder: (context, index) {
                   Map element = this.trailers[index];
                   return Container(
-                    margin: const EdgeInsets.only(right: 15.0,),
+                    margin: const EdgeInsets.only(
+                      right: 15.0,
+                    ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
@@ -66,9 +144,8 @@ class Shows extends StatelessWidget {
                           width: screen.width * 0.60,
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                              image: AssetImage(element['cover']),
-                              fit: BoxFit.fitWidth
-                            ),
+                                image: AssetImage(element['cover']),
+                                fit: BoxFit.fitWidth),
                           ),
                         ),
                         Container(
