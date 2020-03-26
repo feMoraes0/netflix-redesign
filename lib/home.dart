400f65d55b2dc1c11e387adf67d18e6f2eca74fe
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-26 00:07:16 +0000

    feat: filter modal

--- lib/home.dart
+++ lib/home.dart
@@ -22,6 +22,135 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Widget modalElement(String label, {bool active = false}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 15.0,
+      ),
+      margin: const EdgeInsets.only(
+        right: 12.0,
+        bottom: 15.0,
+      ),
+      decoration: BoxDecoration(
+        color: (active) ? Colors.red : Colors.white12,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  void showModal() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Color.fromRGBO(34, 34, 34, 1.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.white60,
+                          ),
+                        ),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.filter_list,
+                            size: 34.0,
+                            color: Colors.white,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              'Filter',
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        this.modalElement(
+                          'Action',
+                          active: true,
+                        ),
+                        this.modalElement('Anime'),
+                        this.modalElement('Asian'),
+                        this.modalElement('British'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        this.modalElement(
+                          'Comedies',
+                          active: true,
+                        ),
+                        this.modalElement('Crime'),
+                        this.modalElement('Docuseries'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        this.modalElement('Dramas'),
+                        this.modalElement('European'),
+                        this.modalElement(
+                          'Horror',
+                          active: true,
+                        ),
+                        this.modalElement('Kids'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        this.modalElement('Reality & Talk Shows'),
+                        this.modalElement('Romance'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        this.modalElement('Sci-Fi & Fantasy'),
+                        this.modalElement('Teen'),
+                        this.modalElement('Thriller'),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 40.0,
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(bottom: 10.0),
+                  child: FlatButton(
+                    color: Colors.white60,
+                    onPressed: () {},
+                    child: Text('Save'),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -84,7 +213,6 @@ class _HomeState extends State<Home> {
                       ),
                     ),
                     Tab(
-                      
                       child: Text(
                         "My List",
                         style: TextStyle(
@@ -98,7 +226,9 @@ class _HomeState extends State<Home> {
               Container(
                 width: 80.0,
                 child: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    this.showModal();
+                  },
                   icon: Icon(
                     Icons.filter_list,
                     size: 30.0,
